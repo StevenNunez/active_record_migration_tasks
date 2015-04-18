3f17e2236f78efb428277a0054b455ae75521ee5
Steven Nunez <steven.nunez@gmail.com>
2015-04-18 12:23:39 -0400

    Load AR 4 tasks
    Some tasks don't seem to be working right now, like db:drop

--- lib/active_record_migration_tasks.rb
+++ lib/active_record_migration_tasks.rb
@@ -1,5 +1,17 @@
+require 'active_record'
 require "active_record_migration_tasks/version"
 
 module ActiveRecordMigrationTasks
-  # Your code goes here...
+  def self.included(_)
+    seed_loader = Object.new
+    def seed_loader.load_seed
+      Kernel.load "db/seeds.rb"
+    end
+
+    ActiveRecord::Tasks::DatabaseTasks.db_dir = 'db'
+    ActiveRecord::Tasks::DatabaseTasks.migrations_paths = 'db/migrate'
+    ActiveRecord::Tasks::DatabaseTasks.seed_loader = seed_loader
+    ActiveRecord::Tasks::DatabaseTasks.env = ENV["DB_ENV"] || 'development'
+    Kernel.load 'active_record/railties/databases.rake'
+  end
 end
